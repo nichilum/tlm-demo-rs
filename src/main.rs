cecd3301b79f26d4e1daf8d60db6397880f54db6
Leo Kling <leo.kling.info@gmail.com>
2023-10-28 16:54:07 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use std::f64::consts::PI;
 
 use bevy::prelude::*;
 use bevy_pixel_buffer::prelude::*;
-use na::{coordinates::X, vector, Matrix4, Vector4};
-use rayon::{array, prelude::*};
+use na::{vector, Matrix4, Vector4};
+use rayon::prelude::*;
 
 extern crate nalgebra as na;
 
-const SIMULATION_WIDTH: u32 = 700;
-const SIMULATION_HEIGHT: u32 = 700;
-const PIXEL_SIZE: u32 = 1;
+const SIMULATION_WIDTH: u32 = 100;
+const SIMULATION_HEIGHT: u32 = 100;
+const PIXEL_SIZE: u32 = 10;
 
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 enum NodeType {
@@ -102,10 +102,10 @@ fn main() {
         pixel_size: UVec2::new(PIXEL_SIZE, PIXEL_SIZE),
     };
 
-    // let mut grid = Grid(vec![
-    //     Node::default();
-    //     (SIMULATION_WIDTH * SIMULATION_HEIGHT) as usize
-    // ]);
+    let mut grid = Grid(vec![
+        Node::default();
+        (SIMULATION_WIDTH * SIMULATION_HEIGHT) as usize
+    ]);
     // grid.set(
     //     SIMULATION_WIDTH / 2,
     //     SIMULATION_HEIGHT / 2,
@@ -115,30 +115,8 @@ fn main() {
     //     },
     // );
 
-    // ACHTUNG BAUSTELLE !!!!
-
-    let mut grid = GridFloat(
-        vec![0.; (SIMULATION_WIDTH * SIMULATION_HEIGHT * NUM_INDEX) as usize],
-        vec![array_pos(50, 50, 0)],
-        vec![],
-    );
-
-    for x in 1..SIMULATION_WIDTH {
-        grid.2.push(array_pos(x, 25, 0));
-    }
-
-    for x in 1..SIMULATION_WIDTH / 10 {
-        grid.1.push(array_pos(
-            x * SIMULATION_WIDTH / 10,
-            SIMULATION_WIDTH / 2,
-            0,
-        ));
-    }
-
-    // BAUSTELLE ENDE !!!!
-
-    // let source = grid.get_mut(SIMULATION_WIDTH / 2, SIMULATION_HEIGHT / 2);
-    // source.current = vector![1., 0., 0., 0.];
+    let source = grid.get_mut(SIMULATION_WIDTH / 2, SIMULATION_HEIGHT / 2);
+    source.current = vector![1., 0., 0., 0.];
 
     let gradient = GradientResource(colorgrad::magma());
 
@@ -149,7 +127,7 @@ fn main() {
         .add_systems(Startup, pixel_buffer_setup(size))
         // .add_systems(FixedUpdate, update_nodes_system)
         // .add_systems(PostUpdate, draw_colors_system)
-        .add_systems(Update, (full_grid_update, draw_pixels))
+        .add_systems(Update, (update_nodes_system, draw_colors_system))
         .run();
 }
 
@@ -188,14 +166,15 @@ fn update_nodes_system(mut grid: ResMut<Grid>, time: Res<Time>) {
         let top = grid.get(x, y - 1);
         let bottom = grid.get(x, y + 1);
 
-        // if x as u32 == SIMULATION_WIDTH / 2 && y as u32 == (SIMULATION_HEIGHT / 2) - 1 {
-        //     info!("{:?}", grid.0[i])
-        // }
+        if x as u32 == SIMULATION_WIDTH / 2 && y as u32 == (SIMULATION_HEIGHT / 2) - 1 {
+            info!("{:?}", grid.0[i])
+        }
 
         let node = grid.0[i].clone();
+        grid.0[i].next = node.calc(left, right, top, bottom);
         match node.node_type {
             NodeType::Source => sin_source(time.elapsed_seconds_f64(), &mut grid),
-            _ => grid.0[i].next = node.calc(left, right, top, bottom),
+            _ => {},
         }
     });
 
@@ -211,125 +190,3 @@ fn sin_source(t: f64, grid: &mut ResMut<Grid>) {
 }
 
 // ACHTUNG BAUSTELLE !!!!
-
-const NUM_INDEX: u32 = 9; //cur_bottom cur_left cur_top cur_right next_bottom next_left next_top next_right pressure
-
-#[derive(Debug, Resource)]
-struct GridFloat(Vec<f32>, Vec<usize>, Vec<usize>); //full grid, sources (1d coords), walls (1d coords)
-
-impl GridFloat {
-    fn update_grid(&mut self) -> () {
-        for i in 0..SIMULATION_WIDTH * SIMULATION_HEIGHT {
-            let array_pos: usize = (i * NUM_INDEX) as usize;
-
-            self.0[array_pos] = self.0[array_pos + 4];
-            self.0[array_pos + 1] = self.0[array_pos + 5];
-            self.0[array_pos + 2] = self.0[array_pos + 6];
-            self.0[array_pos + 3] = self.0[array_pos + 7];
-
-            //pressure
-            self.0[array_pos + 8] = 0.5
-                * (self.0[array_pos]
-                    + self.0[array_pos + 1]
-                    + self.0[array_pos + 2]
-                    + self.0[array_pos + 3]);
-        }
-    }
-
-    fn calc_grid(&mut self) -> () {
-        for x in 1..SIMULATION_WIDTH - 1 {
-            for y in 1..SIMULATION_HEIGHT - 1 {
-                GridFloat::calc(
-                    self,
-                    array_pos(x, y, 0),
-                    self.0[array_pos(x, y + 1, 2)],
-                    self.0[array_pos(x - 1, y, 3)],
-                    self.0[array_pos(x, y - 1, 0)],
-                    self.0[array_pos(x + 1, y, 1)],
-                );
-            }
-        }
-    }
-
-    fn calc(
-        &mut self,
-        coord_one_d: usize,
-        bottom_top: f32,
-        left_right: f32,
-        top_bottom: f32,
-        right_left: f32,
-    ) -> () {
-        self.0[coord_one_d + 4] = 0.5 * (-bottom_top + left_right + top_bottom + right_left);
-        self.0[coord_one_d + 5] = 0.5 * (bottom_top - left_right + top_bottom + right_left);
-        self.0[coord_one_d + 6] = 0.5 * (bottom_top + left_right - top_bottom + right_left);
-        self.0[coord_one_d + 7] = 0.5 * (bottom_top + left_right + top_bottom - right_left);
-    }
-
-    fn apply_sources(&mut self, time: f32) -> () {
-        let sin_calc = (time * 10.).sin();
-        for &i in self.1.iter() {
-            self.0[i + 4] = sin_calc;
-            self.0[i + 5] = sin_calc;
-            self.0[i + 6] = sin_calc;
-            self.0[i + 7] = sin_calc;
-        }
-    }
-
-    fn apply_walls(&mut self) -> () {
-        for &i in self.2.iter() {
-            let (x, y) = array_pos_rev(i as u32);
-            self.0[i + 4] = self.0[array_pos(x, y + 1, 2)];
-            self.0[i + 5] = self.0[array_pos(x - 1, y, 3)];
-            self.0[i + 6] = self.0[array_pos(x, y - 1, 0)];
-            self.0[i + 7] = self.0[array_pos(x + 1, y, 1)];
-        }
-    }
-}
-
-fn array_pos(x: u32, y: u32, index: u32) -> usize {
-    return (y * SIMULATION_WIDTH * NUM_INDEX + x * NUM_INDEX + index) as usize;
-}
-
-fn array_pos_rev(i: u32) -> (u32, u32) {
-    let x = (i / 9) % SIMULATION_WIDTH;
-    let y = i / 9 / SIMULATION_WIDTH;
-    return (x, y);
-}
-
-fn full_grid_update(mut grid: ResMut<GridFloat>, time: Res<Time>) -> () {
-    grid.calc_grid();
-
-    // let sin_calc: f32 = (time.elapsed_seconds() * 10.).sin();
-    // grid.apply_sources(sin_calc);
-    grid.apply_sources(time.elapsed_seconds());
-    grid.apply_walls();
-
-    grid.update_grid();
-}
-
-fn draw_pixels(mut pb: QueryPixelBuffer, grid: Res<GridFloat>, _gradient: Res<GradientResource>) {
-    let mut frame = pb.frame();
-    frame.per_pixel_par(|coords, _| {
-        let p = grid.0[array_pos(coords.x, coords.y, 8) as usize];
-        // let color = gradient.0.at(p);
-        Pixel {
-            r: (p * 255.) as u8,
-            g: (p * 255.) as u8,
-            b: (p * 255.) as u8,
-            a: 255,
-        }
-    });
-
-    for &i in grid.2.iter() {
-        let (x, y) = array_pos_rev(i as u32);
-        frame.set(
-            UVec2::new(x, y),
-            Pixel {
-                r: (255) as u8,
-                g: (0) as u8,
-                b: (0) as u8,
-                a: 255,
-            },
-        );
-    }
-}
